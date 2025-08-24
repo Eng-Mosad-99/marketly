c3885cf25a8ae03d7aa8db7c292e80631e0a714a
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-08-24 15:51:26 +0300

    add color manager

---
+++ lib/core/utils/color_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const primaryColor = Color(0xFF004182);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const blackColor = Color(0xFF000000);
+  static const grey1Color = Color(0xFF808080);
+  static const descriptionColor = Color(0xFF06004F);
+  static const textColor = Color(0xFF06004F);
+}
